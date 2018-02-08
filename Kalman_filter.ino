92c404f63a764b28d7f790ab8d52ad2c7aa74175
Barun <barunbasnet1@gmail.com>
2018-02-08 14:48:09 +0900

    Update Kalman_filter.ino

--- Kalman_filter.ino
+++ Kalman_filter.ino
@@ -2,10 +2,12 @@
 
 // Kalman Filter application (Barun Basnet)
 // Initialization of Kalman Variables
-float R = 40, Q = 1e-1;
-double Xpe0 = 0.0, Xe1 = 0.0;
-double Ppe0 = 0.0, Pp1 = 40, P0 = 0, K = 0.0;      
-double Xe0 = 0.0, Z = 0.0; 
+float R = 40, Q = 1e-1;  //Q = process noise covariance, R = measurement noise covariance
+double Xpe0 = 0.0;  // Xpe0 = prior estimation of signal X at time t=0 (current state)
+double Xe1 = 0.0;  //Xe1 = estimation of X at time t=1 (previous state)
+double Ppe0 = 0.0;  //Ppe0 = prior estimation of "error covariance" at t=0,  
+double Pp1 = 0.0, P0 = 0; //Pp1 = error covariance at t=1, P0 = error covariance at t=0
+double  K = 0.0, Xe0 = 0.0, Z = 0.0; //K = Kalman gain, Xe0 = estimation of signal at t=0, Z = measured signal at t=0
 
 void setup() {
   Serial.begin(9600);
@@ -16,13 +18,13 @@ void loop() {
   Z = analogRead(A0);
   Serial.print(Z, 4);Serial.print("\t");
 
- Xpe0 = Xe1; 
- Ppe0 = Pp1 + Q;
- K = Ppe0/(Ppe0 + R);
- Xe0 = Xpe0 + K * (Z - Xpe0);
- P0 = (1 - K) * Ppe0;
+ Xpe0 = Xe1; //Assumption or prediction 1
+ Ppe0 = Pp1 + Q; //Assumption or prediction 2
+ K = Ppe0/(Ppe0 + R); // Measurement update or correction of "Kalman gain"
+ Xe0 = Xpe0 + K * (Z - Xpe0); // Measurement update or correction of "estimated signal"
+ P0 = (1 - K) * Ppe0; // Measurement update or correction of "error covariance"
  Serial.println(Xe0, 4);
- Xe1 = Xe0;
- Pp1 = P0;
+ Xe1 = Xe0; //Update: current t=0 becomes t=1 in the next step
+ Pp1 = P0; //Update: current t=0 becomes t=1 in the next step
  delay(50);
 }
