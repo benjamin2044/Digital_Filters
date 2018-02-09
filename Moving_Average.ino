bc3aa1c60c29b3e0b7c09ef96bae65c2a091c952
Barun <barunbasnet1@gmail.com>
2018-02-09 20:51:56 +0900

    Update Moving_Average.ino

--- Moving_Average.ino
+++ Moving_Average.ino
@@ -1,29 +1,30 @@
-//Moving Average
+#include <SoftwareSerial.h>
 
-int readings[15];      // the readings from the analog input
+//Moving Average
+double readings[15];      // the readings from the analog input
 int index = 0;         // the index of the current reading
-int total = 0;        // the running total
-int average = 0;      // the average
+double total = 0;        // the running total
+double average = 0;      // the average
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
   // initialize all the elements of the array "readings[15]"
   for (int i = 0; i < 15; i++) { 
     readings[i] = 0; }
 }
 
 void loop() {
-  total = total - readings[index]; // subtract the last reading:
-  readings[index] = analogRead(0); // read from the sensor:
-  total = total + readings[index];  // add the reading to the total:
+  total = total - readings[index];  // subtract the last reading:
+  int sen_val = analogRead(0); //Read from the sensor
+  readings[index] =  sen_val * 5.0/1023.0;
+  total = total + readings[index]; // add the reading to the total:
   index++; // advance to the next position in the array:
-  
-  // if we're at the end of the array...
-  if (index >= 15) { index = 0;  }
 
-  average = total / 15;  // calculate the average:
+  if (index >= 15) { index = 0;  }  // if we're at the end of the array...
+
+  average = total / 15.0;  // calculate the average:
   
-  Serial.print(readings[index]); Serial.print("\t");
-  Serial.println(average);
-  delay(50); 
+  Serial.print(readings[index] * 10, 4); Serial.print("\t");
+  Serial.println(average * 10, 4);
+  delay(50);    
 }
